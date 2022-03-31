a89122881e70e5745011daf6e370c95532a47a61
Liisa <s2100647@edu.bc.fi>
2022-03-31 10:45:55 +0300

    test popup

--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -19,15 +19,13 @@ export class MarkerService {
     return this.placeData; */
     
 
-
-   return 
+   return ;
    
    /* `` +
     `<div>Place: ${ data.name.fi }</div>` +
     `<div>Address: ${ data.location.address.street_address } ${ data.location.address.postal_code } ${ data.location.address.locality } </div>` +
     `<div>Opening Hours: ${ data.opening_hours.hours[0].weekday_id } ${ data.opening_hours.hours[0].opens } ${ data.opening_hours.hours[0].closes } ${ data.opening_hours.hours[0].open24h }</div>` */
   } 
-
 
   makePlaceMarkers(map: L.Map): void {
     this.http.get(this.places).subscribe((res: any) => {
@@ -41,6 +39,11 @@ export class MarkerService {
         marker.addTo(map);
       }
     });
+  }
+
+  getPlaces(): any {
+    const places = this.http.get('/assets/data/places.json');
+    return places;
   }
  
 }
--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MarkerService } from '../marker.service'; 
+import { Modal } from '../map/modal';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-popup',
@@ -7,10 +9,16 @@ import { MarkerService } from '../marker.service';
   styleUrls: ['./popup.component.css']
 })
 export class PopupComponent implements OnInit {
-
+  public places:any = []; //luo tyhjän arrayn jonne APIsta haetut tiedot tallennetaan
+ 
   constructor(private markerService: MarkerService) { }
 
   ngOnInit(): void {
+    this.getData;
   }
 
+  getData(): void {
+    this.markerService.getPlaces()
+      .subscribe((data:any) => this.places = data);
+  }
 }
